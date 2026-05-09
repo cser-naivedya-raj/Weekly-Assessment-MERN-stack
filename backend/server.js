2ce1e043bafabb838754e976c81236f19a70683b
cser-naivedya-raj <cser.naivedya.raj@gmail.com>
2026-05-09 13:20:05 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,18 +6,21 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "*"
+}));
 
 // Routes
 const noteRoutes = require("./routes/noteRoutes");
+
 app.use("/api/notes", noteRoutes);
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 // Server
 const PORT = process.env.PORT || 5000;
